397308ffea9678bd4b1826b1fde4ad353f76bfc3
TitleHHHH <TitleHHHH@outlook.com>
2025-10-12 22:40:34 +0500

    redesign nav menu

--- todolist/src/App.vue
+++ todolist/src/App.vue
@@ -4,11 +4,37 @@
 
 <template>
   <div class="app-container">
-    <p><strong>Current route path:</strong> {{ $route.fullPath }}</p>
-    <nav class="flex flex-row">
-      <RouterLink to="/">Go to Home</RouterLink>
-      <RouterLink to="/rating">Go to About</RouterLink>
-    </nav>
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Редактор
+            заметок</a>
+          <button class="navbar-toggler" type="button"
+                  data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                  aria-controls="navbarNav" aria-expanded="false"
+                  aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse"
+               id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <RouterLink class="nav-link" to="/"><i
+                    class="bi-house"></i> Главная</RouterLink>
+              </li>
+              <li class="nav-item">
+                <RouterLink class="nav-link" to="/rating"><i
+                    class="bi-star"></i> Оценить</RouterLink>
+              </li>
+              <li class="nav-item">
+                <RouterLink class="nav-link" to="/about"><i
+                    class="bi-person-circle"></i> Об авторе</RouterLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
     <main class="main-content">
       <RouterView />
     </main>
