0ae4ff29dd21a522862ef381c49258ed5ee3d59d
TitleHHHH <TitleHHHH@outlook.com>
2025-10-12 22:34:44 +0500

    add star component

---
+++ todolist/src/components/Star.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  filled: Boolean
+})
+</script>
+
+<template>
+  <i
+      :class="['star', filled ? 'bi-star-fill text-warning' : 'bi-star text-secondary']"
+      style="cursor: pointer;"
+  ></i>
+</template>
+
+<style scoped>
+.star {
+  font-size: 50px;
+  margin-right: 4px;
+}
+</style>
